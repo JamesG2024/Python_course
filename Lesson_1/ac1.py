9162ef5cd5428038aadaa1877bb43585cf69bce0
JamesG2024 <j11mes.g2tuno@gmail.com>
2025-10-18 19:38:01 +0400

    lesson_1 done python

---
+++ Lesson_1/ac1.py
@@ -0,0 +1,2 @@
+print("HEllo world")
+print("My name is James")
---
+++ Lesson_1/ac2.py
@@ -0,0 +1,8 @@
+print("Welcome to the world of programming?")
+print(4)
+print("Hello Python \n")
+print("keywords in pyhton are. . . .")
+import keyword
+print(keyword.kwlist)
+
+print(4+5)
