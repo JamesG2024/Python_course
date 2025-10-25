72e6d25324fc4940003e7053cb6fbf4a9f55403d
JamesG2024 <j11mes.g2tuno@gmail.com>
2025-10-25 19:51:41 +0400

    lesson 3 and 2 done

---
+++ Lesson_3/ac4.py
@@ -0,0 +1,10 @@
+import datetime
+
+current_time = datetime.datetime.now()
+
+
+print ("Time now at greenwich merldain is : ", end = "")
+print ( current_time)
+
+import calendar
+print ("\n", calendar.calendar(2024))
